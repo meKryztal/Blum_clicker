e86d4a8eb4dff91dae5928d00ba892c46a8e7c5b
Bodya <47853767+meKryztal@users.noreply.github.com>
2024-07-31 16:45:41 +0300

    Update new_blum.py

--- new_blum.py
+++ new_blum.py
@@ -11,6 +11,8 @@ from tkinter import simpledialog
 
 mouse = Controller()
 
+click_counts = {'6': 2}  # Установить количество игр, дефолт 2 игры
+
 
 def get_window(window):
     windows = pyautogui.getWindowsWithTitle(window)
@@ -23,7 +25,6 @@ def get_window(window):
         return region_left, region_top, region_width, region_height
     else:
         raise Exception(f"Окно '{window}' не найдено.")
-
 
 star_templates_10s = [
     ('6', cv2.imread('6.png', cv2.IMREAD_COLOR)),
@@ -41,34 +42,27 @@ star_templates = [
     ('3', cv2.imread('3.png', cv2.IMREAD_COLOR)),
 ]
 
-
 def click(xs, ys):
     mouse.position = (xs, ys)
     mouse.press(Button.left)
     mouse.release(Button.left)
     time.sleep(0.0001)
 
-
 def choose_window_gui():
     root = tk.Tk()
     root.withdraw()
-
     windows = gw.getAllTitles()
     if not windows:
         return None
-
     choice = simpledialog.askstring("Выбор окна Telegram", "Введите номер окна:\n" + "\n".join(
         f"{i}: {window}" for i, window in enumerate(windows)))
-
     if choice is None or not choice.isdigit():
         return None
-
     choice = int(choice)
     if 0 <= choice < len(windows):
         return windows[choice]
     else:
         return None
-
 
 def grab_screen(region, scale_factor=0.5):
     screenshot = pyautogui.screenshot(region=region)
@@ -78,7 +72,6 @@ def grab_screen(region, scale_factor=0.5):
     new_height = int(screenshot.shape[0] * scale_factor)
     resized_screenshot = cv2.resize(screenshot, (new_width, new_height))
     return resized_screenshot
-
 
 def find_template_on_screen(template, screenshot, step=0.7, scale_factor=0.5):
     new_width = int(template.shape[1] * scale_factor)
@@ -90,17 +83,12 @@ def find_template_on_screen(template, screenshot, step=0.7, scale_factor=0.5):
         return (int(max_loc[0] / scale_factor), int(max_loc[1] / scale_factor))
     return None
 
-
 def click_on_screen(position, template_width, template_height, region_left, region_top):
-
     center_x = position[0] + template_width // 2
     center_y = position[1] + template_height // 2
-    click(center_x + region_left, center_y + region_top+4)
+    click(center_x + region_left, center_y + region_top + 4)
 
-
-
-def process_template(template_data, screenshot, scale_factor, region_left, region_top):
-
+def process_template(template_data, screenshot, scale_factor, region_left, region_top, click_counts):
     template_name, template = template_data
     if template is None:
         print(f"Ошибка загрузки {template_name}")
@@ -108,12 +96,16 @@ def process_template(template_data, screenshot, scale_factor, region_left, regio
     position = find_template_on_screen(template, screenshot, scale_factor=scale_factor)
     if position:
         template_height, template_width, _ = template.shape
-        click_on_screen(position, template_width, template_height, region_left, region_top)
+        if template_name == '6' and click_counts['6'] > 1:
+            click_on_screen(position, template_width, template_height, region_left, region_top)
+            click_counts['6'] -= 1
 
+        elif template_name != '6':
+            click_on_screen(position, template_width, template_height, region_left, region_top)
         return template_name, position
     return template_name, None
 
-
+# Main script
 window_name = "TelegramDesktop"
 check = gw.getWindowsWithTitle(window_name)
 
@@ -131,12 +123,11 @@ paused = True
 last_check_time = time.time()
 last_blue_check_time = time.time()
 last_pause_time = time.time()
-
 last_check_time_10s = time.time()
 last_check_time_5s = time.time()
+end_time = None
 
 while True:
-
     if keyboard.is_pressed('S') and time.time() - last_pause_time > 0.1:
         paused = not paused
         last_pause_time = time.time()
@@ -158,28 +149,32 @@ while True:
             telegram_window.minimize()
             telegram_window.restore()
 
-    if not paused:
-
+    if not paused and click_counts['6'] > 0:
         screenshot = grab_screen(window_rect)
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             current_time = time.time()
 
             if current_time - last_check_time_10s >= 5:
-                futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top) for template_data in star_templates_10s]
+                futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top, click_counts) for template_data in star_templates_10s]
                 last_check_time_10s = current_time
 
             # Удалить эти три строки, если не нужна заморозка и вверху скрипта удалить
             if current_time - last_check_time_5s >= 1:
-                futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top) for template_data in star_templates_5s]
+                futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top, click_counts) for template_data in star_templates_5s]
                 last_check_time_5s = current_time
 
-
-            futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top) for template_data in star_templates]
+            futures += [executor.submit(process_template, template_data, screenshot, 0.5, telegram_window.left, telegram_window.top, click_counts) for template_data in star_templates]
 
             for future in concurrent.futures.as_completed(futures):
                 template_name, position = future.result()
 
+    if click_counts['6'] == 1:
+        if not end_time:
+            end_time = time.time() + 40
+            print('Достигнуто заданное количество игр')
 
+    if end_time and time.time() >= end_time:
+        break
 
 print('Стоп')
