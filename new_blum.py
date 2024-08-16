d13a9e0ebf47bad260385b5c494b56e9d18e4a5d
Bodya <47853767+meKryztal@users.noreply.github.com>
2024-08-16 14:13:58 +0300

    Update new_blum.py

--- new_blum.py
+++ new_blum.py
@@ -8,7 +8,8 @@ from pynput.mouse import Button, Controller
 import pygetwindow as gw
 import tkinter as tk
 from tkinter import simpledialog
-
+import base64
+from colorama import Fore
 mouse = Controller()
 
 
