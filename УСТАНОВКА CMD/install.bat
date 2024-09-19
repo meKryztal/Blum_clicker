03b3a1a81fb6d4b8673caa932b7f00da3786435e
Bodya <47853767+meKryztal@users.noreply.github.com>
2024-09-19 21:42:31 +0300

    Delete УСТАНОВКА CMD directory

--- УСТАНОВКА CMD/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git clone https://github.com/meKryztal/Blum-clicker.git
-cd Blum-clicker
-pip install -r requirements.txt
-
--- УСТАНОВКА CMD/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd Blum-clicker
-python new_blum.py
-
